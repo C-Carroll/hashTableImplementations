2fef574977871685edda805e9e855b0fe0920e6a
christine parsons <clong9712@gmail.com>
2023-01-26 19:37:01 -0500

    resize finished

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -9,8 +9,10 @@ class KeyValuePair {
 class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   constructor(numBuckets = 8) {
-    // Initialize your buckets here
-    // Your code here
+    this.count = 0;
+    this.capacity = numBuckets;
+    this.data = new Array(this.capacity).fill(null)
+
   }
 
   hash(key) {
@@ -24,24 +26,58 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
   }
 
   hashMod(key) {
-    // Get index after hashing
     return this.hash(key) % this.capacity;
   }
 
 
   insert(key, value) {
-    // Your code here
+    let index = this.hashMod(key);
+    let curr = this.data[index];
+    while (curr) {
+      if (curr.key === key) {
+        curr.value = value;
+        return;
+      }
+      curr = curr.next
+    }
+    let newNode = new KeyValuePair(key, value);
+    newNode.next = this.data[index]
+    this.data[index] = newNode;
+    this.count++
   }
 
 
   read(key) {
-    // Your code here
+    let arrPos = this.data[this.hashMod(key)]
+    while (arrPos.next && arrPos.key !== key) {
+      arrPos = arrPos.next
+    }
+    if(arrPos.key === key){
+    return arrPos.value}
+    else{
+      return undefined
+    }
   }
 
 
   resize() {
-    // Your code here
-  }
+    let old = this.data.slice();
+    this.capacity = this.capacity * 2
+    this.data = new Array(this.capacity).fill(null);
+    this.count = 0
+
+    console.log(old + "--old array--")
+    for(let i = 0; i < old.length; i++){
+      let pos = old[i]
+      while(pos) {
+        this.insert(pos.key, pos.value)
+        pos = pos.next
+      }
+
+      console.log(this.data + "--- new arr---")
+    }
+   }
+  
 
 
   delete(key) {
